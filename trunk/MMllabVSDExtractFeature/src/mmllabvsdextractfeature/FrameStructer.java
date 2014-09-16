9693add4b3b5e05d73589019f4a3437c7378d2b5
tiendo.vn@gmail.com <tiendo.vn@gmail.com@8bfe0158-79d5-477d-24f1-8d653a2ebfd0>
2014-09-16 13:58:14 +0000

    Test unzip file

---
+++ trunk/MMllabVSDExtractFeature/src/mmllabvsdextractfeature/FrameStructer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mmllabvsdextractfeature;
+
+/**
+ *
+ * @author tiendv
+ */
+   /**
+     * Example devel2011-new/VSD_devel2011_1_001/VSD_devel2011_1.shot_1.RKF_1.Frame_1
+     * 
+     * VSD_devel2011_1_001: zipFileName
+     * devel2011-new: wholeFolderName content all zipFile
+     * VSD_devel2011_1: folder
+     * shot_1: shot and id shot
+     * Frame_1: frame and id frame
+     */
+
+public class FrameStructer {
+    String folder;
+    String shotID;
+    String frameID;
+}
